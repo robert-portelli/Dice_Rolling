25d7dcd4f0cb755b6c702876c5afb5a68f79f922
Robert Portelli <github@robertportelli.com>
2023-11-18 14:59:25 -0500

    Fixes #6

--- dice.py
+++ dice.py
@@ -1,5 +1,7 @@
 # dice.py
 import random
+import tomllib
+import pathlib
 
 
 def parse_input(input_string):
@@ -21,12 +23,42 @@ def roll_dice(num_dice):
     pass
     """
 
-    return [random.randint(1, 6) for _ in range(num_dice)]
+    return [random.randint(0, 5) for _ in range(num_dice)]
 
+
+DICE_ART_PATH = pathlib.Path(__file__).parent / "pyproject.toml"
+DICE_ART = tomllib.loads(DICE_ART_PATH.read_text())["DICE_ART"]["faces"]
+DIE_HEIGHT = len(DICE_ART[1])
+DIE_WIDTH = len(DICE_ART[1][0])
+DIE_FACE_SEPARATOR = " "
 
 # ~~~ App's main code block ~~~
 # 1. Get and validate user's input
 num_dice_input = input("How many dice do you want to roll? [1-6] ")
-num_dice = parse_input(num_dice_input)
+dice_to_roll = parse_input(num_dice_input)
 # 2. Roll the dice
-roll_results = roll_dice(num_dice)
+roll_results = roll_dice(dice_to_roll)
+
+"""
+Return the dice art corresponding to each index in roll_results:
+list of lists of strings
+"""
+faces = [DICE_ART[face] for face in roll_results]
+
+
+# Generate a list containing the dice faces rows
+face_rows = []
+for row_idx in range(DIE_HEIGHT):
+    row_components = []
+    for face in faces:
+        row_components.append(face[row_idx])
+    row_string = DIE_FACE_SEPARATOR.join(row_components)
+    face_rows.append(row_string)
+
+
+# Generate header with the word "RESULTS" centered
+width = len(face_rows[0])
+diagram_header = " RESULTS ".center(width, "~")
+
+dice_faces_diagram = "\n".join([diagram_header] + face_rows)
+print(dice_faces_diagram)
